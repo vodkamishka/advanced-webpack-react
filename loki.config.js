695508f72b78ad35e275c316fc483d864044fc8b
deynega <deynega.a@2wtrade.com>
2025-01-27 16:14:38 +0700

    add loki

---
+++ loki.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "configurations": {
+        "chrome.laptop": {
+            "target": "chrome.app",
+            "width": 1366,
+            "height": 768,
+            "deviceScaleFactor": 1,
+            "mobile": false
+        },
+        "chrome.iphone7": {
+            "target": "chrome.app",
+            "preset": "iPhone 7"
+        }
+    },
+    storybook: {
+        port: 6006,       // Порт Storybook
+        host: '127.0.0.1', // Хост Storybook
+        configDir: './config/storybook', // Путь к конфигурации Storybook
+    },
+};
