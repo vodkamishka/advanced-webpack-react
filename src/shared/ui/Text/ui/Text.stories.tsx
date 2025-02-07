e6336397f63c8031e0d46e97cb8f023f07fef0e0
deynega <deynega.a@2wtrade.com>
2025-02-07 21:20:05 +0700

    add ui component Text

---
+++ src/shared/ui/Text/ui/Text.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Text } from 'shared/ui/Text';
+import { TextTheme } from 'shared/ui/Text/ui/Text';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+    title: 'Text',
+    component: Text,
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+        layout: 'centered',
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+    tags: ['autodocs']
+} satisfies Meta<typeof Text>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+    args: {
+        title: 'Primary',
+        text: 'Primary',
+        theme: TextTheme.PRIMARY,
+    },
+};
+export const Error: Story = {
+    args: {
+        title: 'Error',
+        text: 'Error',
+        theme: TextTheme.ERROR,
+    },
+};
+
+export const OnlyTitle: Story = {
+    args: {
+        title: 'Only title',
+    },
+};
+export const OnlyText: Story = {
+    args: {
+        text: 'Only text',
+    },
+};
+
+
+
+
+
