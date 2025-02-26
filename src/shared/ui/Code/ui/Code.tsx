1307864d94483c7f896404442dbeb4ff56034718
deynega <deynega.a@2wtrade.com>
2025-02-26 12:51:50 +0700

    create new component Code

---
+++ src/shared/ui/Code/ui/Code.tsx
@@ -0,0 +1,29 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo, useCallback } from 'react';
+import CopyIcon from 'shared/assets/icons/copy-20-20.svg';
+import cls from './Code.module.scss';
+import { Button, ButtonTheme } from 'shared/ui/Button';
+
+interface CodeProps {
+    className?: string;
+    text: string;
+}
+
+export const Code = memo(function Code(props: CodeProps) {
+    const { className, text } = props;
+
+    const onCopy = useCallback(() => {
+        navigator.clipboard.writeText(text);
+    }, [text]);
+
+    return (
+        <pre className={classNames(cls.code, {}, [className])}>
+            <Button onClick={onCopy} className={cls.copyBtn} theme={ButtonTheme.CLEAR}>
+                <CopyIcon className={cls.copyIcon} />
+            </Button>
+            <code>
+                {text}
+            </code>
+        </pre>
+    );
+});
