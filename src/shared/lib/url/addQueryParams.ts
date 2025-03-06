ea7b2175e0d14979a209e0020388a358b202e3f4
deynega <deynega.a@2wtrade.com>
2025-03-06 11:33:10 +0700

    create function addQueryParam for adding searching params to url

---
+++ src/shared/lib/url/addQueryParams.ts
@@ -0,0 +1,17 @@
+export function getQueryParams(params: Record<string, string>) {
+    const searchParams = new URLSearchParams(window.location.search);
+    Object.entries(params).forEach(([name, value]) => {
+        if (value !== undefined) {
+            searchParams.set(name, value);
+        }
+    });
+    return `?${searchParams.toString()}`;
+}
+
+/**
+ * Функция добавления параметров строки запроса в URL
+ * @param params
+ */
+export function addQueryParams(params: Record<string, string>) {
+    window.history.pushState(null, '', getQueryParams(params));
+}
